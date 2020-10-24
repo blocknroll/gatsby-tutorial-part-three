1e2c6e34c5c1834167a7c49db4f9259ddc783bf6
Fred Block <5204778+blocknroll@users.noreply.github.com>
2020-10-24 12:46:17 -0500

    Add form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,6 +8,35 @@ export default function Contact() {
       <p>
         <a href="mailto:me@example.com">me@example.com</a>
       </p>
+      <form name="contact-form-test" method="POST" data-netlify="true">
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Role:{" "}
+            <select name="role[]" multiple>
+              <option value="widget-maker">Widget Maker</option>
+              <option value="widget-seller">Widget Seller</option>
+            </select>
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </Layout>
   )
 }
