13dd846b291831744948728338605e0ab9824522
Fred Block <5204778+blocknroll@users.noreply.github.com>
2020-10-24 17:00:23 -0500

    Add redirect action to form. Add src/pages/success.js

---
+++ src/pages/success.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function Success() {
+  return (
+    <Layout>
+      <h1>SUCCESS</h1>
+      <p>This is so rad! You're going to be flying soon!</p>
+      <p>insert pic of Chris smiling here...</p>
+    </Layout>
+  )
+}
