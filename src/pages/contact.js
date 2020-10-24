dd10a63d502b159e9cde6d9f142781b5221383b6
Fred Block <5204778+blocknroll@users.noreply.github.com>
2020-10-24 13:26:59 -0500

    Update form. Add netlify attribute to form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,10 +5,8 @@ export default function Contact() {
   return (
     <Layout>
       <h1>I'd love to talk! Email me at the address below</h1>
-      <p>
-        <a href="mailto:me@example.com">me@example.com</a>
-      </p>
-      <form name="contact-form-test" method="POST" data-netlify="true">
+
+      <form name="contact-form-test-2" netlify>
         <p>
           <label>
             Your Name: <input type="text" name="name" />
@@ -17,20 +15,6 @@ export default function Contact() {
         <p>
           <label>
             Your Email: <input type="email" name="email" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Role:{" "}
-            <select name="role[]" multiple>
-              <option value="widget-maker">Widget Maker</option>
-              <option value="widget-seller">Widget Seller</option>
-            </select>
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name="message"></textarea>
           </label>
         </p>
         <p>
